271162058919fd52c6a6e5c31a378dc9912953b6
Kaal Purush <kaalbd@gmail.com>
2013-12-08 05:22:14 +0600

    fix

--- nodejs/kommand.js
+++ nodejs/kommand.js
@@ -34,29 +34,29 @@ function cmd_exec(cmd, cb_stdout, cb_end) {
 }
 
 function process_cmd(cmd){
+	cmd=cmd.toLowerCase();
+	if(cmd.indexOf('search')>=0)
+		cmd_exec('start /max http://www.google.com/search?q="'+cmd.replace('search','').replace(/^\s\s*/, '').replace(/\s\s*$/, '').replace(/ /g, '+')+'"');
 
-if(cmd.indexOf('search')>=0)
-	cmd_exec('start /max http://www.google.com/search?q="'+cmd.replace('search','').replace(/^\s\s*/, '').replace(/\s\s*$/, '').replace(/ /g, '+')+'"');
+	if(cmd.indexOf('open')>=0)
+		cmd_exec('start /max http://'+cmd.replace('open','').replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
+		
+	if(cmd.indexOf('run')>=0)
+		cmd_exec('start '+cmd.replace('run','').replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
 
-if(cmd.indexOf('open')>=0)
-	cmd_exec('start /max http://'+cmd.replace('open','').replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
-	
-if(cmd.indexOf('run')>=0)
-	cmd_exec('start '+cmd.replace('run','').replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
+	if(cmd.indexOf('shutdown')>=0 && cmd.indexOf('computer')>=0)
+		cmd_exec('shutdown -p');
 
-if(cmd.indexOf('shutdown')>=0 && cmd.indexOf('computer')>=0)
-	cmd_exec('shutdown -p');
-
-if(cmd.indexOf('restart')>=0 && cmd.indexOf('computer')>=0)
-	cmd_exec('shutdown -r -f -t 0');	
-	
-if(cmd.indexOf('logoff')>=0 || cmd.indexOf('log off')>=0 && cmd.indexOf('computer')>=0)
-	cmd_exec('shutdown -l');
-	
-if(cmd.indexOf('lock')>=0 && cmd.indexOf('computer')>=0 && cmd.indexOf('unlock')<0)
-	cmd_exec('rundll32.exe user32.dll,LockWorkStation');
-	
-if(cmd.indexOf('unlock')>=0 && cmd.indexOf('computer')>=0)
-	setTimeout(function(){cmd_exec('cscript speak.vbs "Please enter your password."');},2000);
+	if(cmd.indexOf('restart')>=0 && cmd.indexOf('computer')>=0)
+		cmd_exec('shutdown -r -f -t 0');	
+		
+	if(cmd.indexOf('logoff')>=0 || cmd.indexOf('log off')>=0 && cmd.indexOf('computer')>=0)
+		cmd_exec('shutdown -l');
+		
+	if(cmd.indexOf('lock')>=0 && cmd.indexOf('computer')>=0 && cmd.indexOf('unlock')<0)
+		cmd_exec('rundll32.exe user32.dll,LockWorkStation');
+		
+	if(cmd.indexOf('unlock')>=0 && cmd.indexOf('computer')>=0)
+		setTimeout(function(){cmd_exec('cscript speak.vbs "Please enter your password."');},2000);
 	
 }
