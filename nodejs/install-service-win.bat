9cc380da3cbdbcbb471a0c6d459ceab9d1cd42c5
Kaal Purush <kaalbd@gmail.com>
2013-12-13 22:39:53 +0600

    remove install-service-win.bat

--- nodejs/install-service-win.bat
+++
@@ -1,3 +0,0 @@
-nssm.exe install kommand "C:\Program Files\nodejs\node.exe" "%~dp0kommand.js"
-reg add "HKLM\SYSTEM\CurrentControlSet\services\kommand\Parameters" /f /v "AppDirectory" /t REG_EXPAND_SZ /d %~dp0
-net start kommand
